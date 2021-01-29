8a425b1e97cb6e3d69a8585bb800c02720779e71
epiphyte <epiphyte@vertex.link>
2021-01-29 17:55:39 +0000

    Tweak checkout

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
+git clone https://github.com/vertexproject/synapse.git -v ./synapse
 cd ./synapse
 git checkout msgpack_prep
 pytest -v -s --disable-warnings --durations 6 --maxfail 6
