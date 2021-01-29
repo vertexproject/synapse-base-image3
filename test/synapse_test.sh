776d7c5ae633f0411fd3229a75ed88fdfc77384c
epiphyte <epiphyte@vertex.link>
2021-01-29 18:14:16 +0000

    Revert the test changes

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/vertexproject/synapse.git -v ./synapse
+git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
-git checkout msgpack_prep
 pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
